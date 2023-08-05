61c07c383a6be58d6bbbf31bc7df5ea7c81c0990
Marichka0701 <tanechnyk.marichka07@gmail.com>
2023-08-05 14:58:19 +0300

    some changes

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDr44-SL-7Lwvp7cek4tRmHL5A2VMrcllA",
-    authDomain: "tripforecast-d3b0d.firebaseapp.com",
-    projectId: "tripforecast-d3b0d",
-    storageBucket: "tripforecast-d3b0d.appspot.com",
-    messagingSenderId: "132664203417",
-    appId: "1:132664203417:web:c94ce707263ec15e8ac008"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
